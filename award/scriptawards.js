0c0bdb3569b1f325b39b40509e480be75132fc01
J-Stiehl <salsajackie@yahoo.com>
2023-07-18 14:32:56 -0500

    adding column content

--- award/scriptawards.js
+++ award/scriptawards.js
@@ -1,27 +1,57 @@
+//First Name Last Name High School Time? Error?
 
-// First Name	Last Name	High Score	time? error?
+function tableGen() {
+  //getting body elements
+  let tableBody = document.getElementsByClassName("table-group-divider");
 
-function tableGen(){
-    let tableBody = document.getElementsByClassName('table-group-divider')
-    //getting<tbody> elements
-    let tableRow = document.createElement('tr')
-    let tableHead = document.createElement('th')
-    let tableCol = document.createElement('td')
-    //creating table elemets
-    tableBody[0].appendChild(tableRow);
-    tableRow.appendChild(tableHead)
-        for(time= 0 ; time <4 ; time ++ ){
-        tableRow.appendChild(tableCol)
-        }
-   console.log(tableCol)
-   tableCol.innerHTML = 'jeb'
-    //appending tableElement into <tbody>
+  //creating table elements
+  let tableRow = document.createElement("tr");
+  let tableHead = document.createElement("th");
+  let tableColumn;
+
+  //appending tableElement into tbody
+  tableBody[0].appendChild(tableRow);
+  tableRow.appendChild(tableHead);
+  //   tableRow.appendChild(tableColumn);
+
+  for (time = 0; time < 4; time++) {
+    let tableColumn = document.createElement("td");
+    tableRow.appendChild(tableColumn);
+    tableColumn.id = time;
+  }
+
+  function getGameData() {
+    var tableInfo = [];
+    tableInfo = JSON.parse(localStorage.getItem("game data"));
+    displayGameData();
+  }
+
+  function addNewGameData() {
+    //going to newjs?
+    tableInfo.push();
+    localStorage.setItem("game data", JSON.stringify(tableInfo));
+    displayGameData();
+  }
+
+  function displayGameData() {
+    // console.log(tableColumn);
+    tableColumn.innerHTML = "first name";
+    // tableColumn.innerHTML = "last name";
+    // tableColumn.innerHTML = "high score";
+    // tableColumn.innerHTML = "awards";
+    console.log(tableColumn);
+  }
+
+  //   jegGen();
+  getGameData();
+  addNewGameData();
+  displayGameData();
 }
 
+// function jegGen() {
+//   console.log(tableColumn);
+//   tableCol[0].innerHTML = "jeb";
+//   element.innerHTML = "stuff to put in";
+// }
 
-function jegGen(){
-    console.log(tableCol)
-
-//element. innerHTML = 'stuff to put in'
-}
-tableGen()
+tableGen();
--- newJs.js
+++ newJs.js
@@ -1,109 +1,101 @@
+// //define raw API data for storage
+// var raw;
+// //this raw doesn't do anything, i don't know why
 
+// async function gainsheet() {
+//   const response = await fetch("https://sudoku-api.vercel.app/api/dosuku");
+//   const rawAPI = await response.json();
+//   setGame(rawAPI);
+//   console.log(rawAPI);
+// }
 
+// window.load = gainsheet();
+// //run as window open
 
-//define raw API data for storage
-var raw;
-//this raw doesn't do anything, i don't know why
+// //asign cordnite for number selected also tile that is selected
+// //error feature for scoring
+// var numberSelected = null;
+// var tileSelected = null;
+// var error = 0;
+// //trying to fucking get the board, it doesn't let me extract the property of raw what the heck
 
-  async function gainsheet() {
-    const response = await fetch("https://sudoku-api.vercel.app/api/dosuku");
-    const rawAPI = await response.json();
-    setGame(rawAPI)
-    console.log(rawAPI)
-  }
+// //start game once window load
+// // window.onload = gainsheet()
 
- window.load = gainsheet()
- //run as window open
+// function setGame(data) {
+//   let userChoice;
+//   let boardSolution = data.newboard.grids[0].solution;
+//   let boardValue = data.newboard.grids[0].value;
+//   console.log(boardValue);
+//   //digit 1-9 input bar
+//   // for (let i = 1; i <= 9; i++){
+//   //   //<div id=i class number> i <div>
+//   //   //for each i repeat <div> above
+//   //   let numberBox = document.createElement('div')
+//   //   numberBox.id = i
+//   //   numberBox.innerText = i
+//   //   //interactive with selectNumber()
+//   //   numberBox.addEventListener('click', function(){
+//   //     userChoice = numberBox.id
 
+//   //     console.log(userChoice)
+//   //     console.log(numberBox.id)
+//   //   });
+//   //   numberBox.classList.add('number')
+//   //   document.getElementById('digits').appendChild(numberBox)
+//   // }
 
-//asign cordnite for number selected also tile that is selected
-    //error feature for scoring
-    var numberSelected = null
-    var tileSelected = null;
-    var error = 0;
-//trying to fucking get the board, it doesn't let me extract the property of raw what the heck
+//   //board 9x9, easy!
+//   for (let r = 0; r < 9; r++) {
+//     //each row and each col
+//     for (let c = 0; c < 9; c++) {
+//       let box = document.createElement("div");
+//       box.id = r + "-" + c;
+//       if (boardValue[r][c] != "0") {
+//         box.innerText = boardValue[r][c];
+//         box.classList.add("box-start");
+//         //skipping 0 in the begining value
+//       } else {
+//         box.innerText = "";
+//       }
+//       if (r == 2 || r == 5) {
+//         box.classList.add("horizontal-line");
+//       }
+//       if (c == 2 || c == 5) {
+//         box.classList.add("vertical-line");
+//         //add line
+//       }
+//       box.addEventListener("click", function () {
+//         let userBoxSelected = document.getElementById(`${box.id}`);
+//         // console.log(`cord box ${box.id}`)
+//         //lets user select number and fill box, need if statement to deny filled boxes
+//         if (this.innerText != "") {
+//           return;
+//         } else {
+//           userBoxSelected.textContent = userChoice;
+//           //Chris - I added this line to show the start of the code I added.  can delete this line for final product
+//           if (userChoice != boardSolution[r][c]) {
+//             error = error + 1;
+//             console.log("bummer dude");
+//             console.log(error);
+//           }
+//         }
+//         // userBoxSelected.textContent = userChoice // this can be deleted for final product
+//       });
 
+//       // console.log(`cord box ${box.id}`));
 
- //start game once window load 
-// window.onload = gainsheet()
+//       box.classList.add("box");
+//       document.getElementById("board").append(box);
+//     }
+//   }
+// }
 
-  
-function setGame(data){
-  let userChoice;
-  let boardSolution = data.newboard.grids[0].solution;
-  let boardValue = data.newboard.grids[0].value;
-  console.log(boardValue)
-  //digit 1-9 input bar
-  for (let i = 1; i <= 9; i++){
-    //<div id=i class number> i <div>
-    //for each i repeat <div> above
-    let numberBox = document.createElement('div')
-    numberBox.id = i
-    numberBox.innerText = i
-    //interactive with selectNumber()
-    numberBox.addEventListener('click', function(){
-      userChoice = numberBox.id
-      
-      console.log(userChoice)
-      console.log(numberBox.id)
-    });
-    numberBox.classList.add('number')
-    document.getElementById('digits').appendChild(numberBox)
-  }
+// //need current selector for user selection
 
-  //board 9x9, easy!
-  for(let r = 0; r < 9; r++){
-    //each row and each col
-    for(let c = 0; c < 9; c++){
-      let box = document.createElement('div');
-        box.id = r + '-' + c;
-        if(boardValue[r][c] != '0'){
-          box.innerText = boardValue[r][c]
-          box.classList.add('box-start')
-          //skipping 0 in the begining value
-        } else {
-          box.innerText = ''
-        }
-        if (r == 2 || r == 5){
-          box.classList.add('horizontal-line')}
-        if (c == 2 || c == 5) {
-          box.classList.add("vertical-line");
-          //add line
-        }
-      box.addEventListener("click", function(){
-        let userBoxSelected = document.getElementById(`${box.id}`)
-        // console.log(`cord box ${box.id}`)
-        //lets user select number and fill box, need if statement to deny filled boxes 
-       if (this.innerText != ''){
-        return
-       } else {
-          userBoxSelected.textContent = userChoice;
-          //Chris - I added this line to show the start of the code I added.  can delete this line for final product
-          if(userChoice != boardSolution[r][c]){
-            error = error +1
-            console.log('bummer dude')
-            console.log(error)
-          }  
-      }
-        // userBoxSelected.textContent = userChoice // this can be deleted for final product
-      });
-          
-          // console.log(`cord box ${box.id}`));
-          
-      box.classList.add("box");
-      document.getElementById("board").append(box);
-      
-    } 
-  }
-}
-
-
-//need current selector for user selection
-
-//function for selectingNumber under 
-function selectNumber(){
-    boardValue = this;
-    boardValue.classList.add('number-selected')
-    console.log(boardValue)
-    }
-
+// //function for selectingNumber under
+// function selectNumber() {
+//   boardValue = this;
+//   boardValue.classList.add("number-selected");
+//   console.log(boardValue);
+// }
