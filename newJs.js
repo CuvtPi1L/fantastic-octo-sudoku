8dfafcd190341bf1db52c8f4a297db185de0ebba
PatK <pkykev@gmail.com>
2023-07-19 10:18:04 -0500

    save work

--- newJs.js
+++ newJs.js
@@ -1,4 +1,5 @@
-
+// current issues: 
+// when page loads user can input 1 but it does not count since the user input is 
 async function gainsheet() {
   const response = await fetch("https://sudoku-api.vercel.app/api/dosuku");
   const rawAPI = await response.json();
@@ -7,23 +8,22 @@ async function gainsheet() {
 window.load = gainsheet()
 //fetch function
 
-// need spot for timer to insert timer text
-  var timerCount = 300;
-  var error = 0;
-  var timer;
-  var fin = false;
-  var saveUserChoice = 1;
-  var gameResultList = [];
+
+var timerCount = 300;
+var error = 0;
+var timer;
+var fin = false;
+var saveUserChoice = 1;
+var gameResultList = [];
 
 var emptyStartTile_Number
 var solvedTile_Number
 
-  endGameJump = document.querySelector('#endGameJump')
-  endGameJump.addEventListener('click', () => 
-    { endGame(); console.log('Cheat-Actv')})
+endGameJump = document.querySelector('#endGameJump')
+endGameJump.addEventListener('click', () => { endGame(); console.log('Cheat-Actv') })
 
 
-let numberBarGeneration = () =>{
+let numberBarGeneration = () => {
   for (let i = 1; i <= 9; i++) {
 
     //<div id=i class number> i <div>
@@ -35,7 +35,7 @@ let numberBarGeneration = () =>{
 
     numberBox.addEventListener('click', function () {
       userChoice = numberBox.id
-      if (saveUserChoice != userChoice){
+      if (saveUserChoice != userChoice) {
         let removeShading = document.getElementById(saveUserChoice)
         removeShading.classList.remove('number-selected')
       }
@@ -48,7 +48,7 @@ let numberBarGeneration = () =>{
   }
 }
 
-function sudokuGridGeneration(){
+function sudokuGridGeneration() {
   for (let r = 0; r < 9; r++) {
     for (let c = 0; c < 9; c++) {
       let box = document.createElement('div');
@@ -75,13 +75,13 @@ function sudokuGridGeneration(){
   console.log(emptyStartTile_Number)
 }
 
-var boardSolution 
-var  boardValue 
+var boardSolution
+var boardValue
 let userChoice;
 
 function setGame(rawAPI) {
-   boardSolution = rawAPI.newboard.grids[0].solution;
-   boardValue = rawAPI.newboard.grids[0].value;
+  boardSolution = rawAPI.newboard.grids[0].solution;
+  boardValue = rawAPI.newboard.grids[0].value;
 
   console.log(boardValue)
   //digit 1-9 input bar
@@ -89,6 +89,7 @@ function setGame(rawAPI) {
   //board 9x9
   sudokuGridGeneration()
   boxempty()
+  timerGo()
 
 
 }
@@ -98,53 +99,56 @@ let boxempty = () => {
   //add eventlistener to each emptybox that has a class .box-empty
   boxStart.forEach(emptyBox => {
     emptyBox.addEventListener("click", () => {
-      if(saveUserChoice != 0){
-      userBoxSelected=document.getElementById(`${emptyBox.id}`)
-     
-      const r = emptyBox.id.split('-')[0]
-      const c = emptyBox.id.split('-')[1]
-      userBoxSelected.textContent = saveUserChoice;
-      //parses the user input into value array to compare for endgame
-      boardValue[r].splice([c], 1, Number.parseInt(userBoxSelected.textContent))
+      if (saveUserChoice != 0) {
+        userBoxSelected = document.getElementById(`${emptyBox.id}`)
 
-      if (userChoice == boardSolution[r][c]) {
-        userBoxSelected.classList.add('solved')
-        try { userBoxSelected.classList.remove('red-text') }
-        catch { console.log('no red text') }
-        
+        const r = emptyBox.id.split('-')[0]
+        const c = emptyBox.id.split('-')[1]
+        userBoxSelected.textContent = saveUserChoice;
+        //parses the user input into value array to compare for endgame
+        boardValue[r].splice([c], 1, Number.parseInt(userBoxSelected.textContent))
+// userboxslected = users chosen box from bottom row however saveuserchoice = 1
+        if (userChoice == boardSolution[r][c]) {
+          userBoxSelected.classList.add('solved')
+          try { userBoxSelected.classList.remove('red-text') }
+          catch { console.log('no red text') }
+
+        }
+        else {
+          reduceTimer(5)
+          error = error + 1
+          userBoxSelected.classList.add('red-text')
+        }
       }
-      else {
-        error = error + 1
-        userBoxSelected.classList.add('red-text')
-      }}
       let solvedTile_Class = '.solved'
-       solvedTile_Number = get_class_Number(solvedTile_Class)
+      solvedTile_Number = get_class_Number(solvedTile_Class)
       //end game condition below
-      if( solvedTile_Number == emptyStartTile_Number){endGame()}
-      });
+      if (solvedTile_Number == emptyStartTile_Number) { endGame() }
+    });
   })
-  }
+}
 
 
-function get_class_Number(className)  {
+function get_class_Number(className) {
   let varClassTag = document.querySelectorAll(className)
   varClassTag_Number = varClassTag.length
+
   return varClassTag_Number
 }
 
 
-function endGame(){
+function endGame() {
   let person = prompt('You finished the sudoku! what is your name!');
   console.log(person)
-  if (person != null){
+  if (person != null) {
     console.log(`${person}!`)
-  }else{
+  } else {
     person = 'Gary Alves'
   }
   //create string of fn, ln, error# and time
   let gameResultData = person.split(' ')
   console.log(gameResultData)
-  gameResultData.push(error,timer)  // not sure if timer is the correct variable for time
+  gameResultData.push(error, timer)  // not sure if timer is the correct variable for time
   console.log(gameResultData)
   //push string to string of strings 
   gameResultList.push(gameResultData)
@@ -154,10 +158,9 @@ function endGame(){
 }
 
 
-function reduceTimer(num){
-  timerCount-=num;
+function reduceTimer(num) {
+  timerCount -= num;
   // _____.textContent = timerCount; you can insert timertext by changing left variable
-  h2timer.textContent = timerCount;
   if (fin && timerCount > 0) {
     clearInterval(timer);
     // score();//need
@@ -170,9 +173,10 @@ function reduceTimer(num){
 
 
 function timerGo() {
-  timer = setInterval(function(){
+  timer = setInterval(function () {
+    document.getElementById('timer').textContent = (`Time left: ${timerCount}`)
     reduceTimer(1);
-  },1000)
+  }, 1000)
 }
 
 //JSON.stringify(k1) === JSON.stringify(k2); // true //this is my compare function for the endgame
